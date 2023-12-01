3ae0dcb21e181b4ca3e52247031da7ec5c7d29d0
cedricfoth <83011086+cedricfoth@users.noreply.github.com>
2023-12-01 18:52:39 +0100

    Updated Code of Simulation
    Due to changes in the simulation, a short update is done.

--- nodes/live_iir_filter.py
+++ nodes/live_iir_filter.py
@@ -13,16 +13,18 @@ class live_iir_filter():
         
     def __call__(self, x):
         y = self.b[0] * x
-        
-        for i in range(self.order):
-            if np.isnan(self.prev_x[i]) or np.isnan(self.prev_y[i]):
+
+        for n in range(self.order):
+            if np.isnan(self.prev_x).any() or np.isnan(self.prev_y).any():
                 y = x
             else:
-                y += self.b[i + 1] * self.prev_x[i]
-                y -= self.a[i + 1] * self.prev_y[i]
-                
+                y += self.b[n + 1] * self.prev_x[n] - self.a[n + 1] * self.prev_y[n]
+        
+        y /= self.a[0]
+
         self.prev_x = np.roll(self.prev_x, 1)
         self.prev_y = np.roll(self.prev_y, 1)
         self.prev_x[0] = x
         self.prev_y[0] = y
-        return y / self.a[0]
+        
+        return y
